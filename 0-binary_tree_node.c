c2eecbcfe0615f2ca90d50e5dc64d024f53e92ee
Ifejaye <iamifejaye@example.com>
2024-02-29 22:27:24 +0100

    return null for failure

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,10 +9,10 @@
   */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode = malloc(sizeof(binary_tree_t))
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
 	if (newNode == NULL)
-		exit(EXIT_FAILURE);
+		return (NULL);
 	newNode->n = value;
 	newNode->parent = parent;
 	newNode->left = NULL;
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
 	if (newNode == NULL)
-		exit(EXIT_FAILURE);
+		return (NULL);
 	newNode->n = value;
 	newNode->parent = parent;
 	newNode->right = NULL;
